2496cf369c06e68b255ad9ac752608f4264a8a77
Jen Merritt <jenjomez@gmail.com>
2020-04-17 16:02:40 +0100

    update routes and sorting

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.models import User, Student
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, url_for
+import random
 
 @app.route('/')
 def root():
@@ -13,10 +14,44 @@ def index():
     students = user.students
     return render_template('index.html', user=user, students=students)
 
+@app.route('/students/<int:student_id>/sorting')
+def sorting(student_id):
+    houses=["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
+    sorted_house = random.choice(houses)
+    student = Student.query.get(student_id)
+    return render_template('sorting.html', student=student, sorted_house=sorted_house)
+
+@app.route('/students/<int:student_id>/sorting', methods=['POST'])
+def sorting_update(student_id):
+    student = Student.query.get(student_id)
+    student.first_name = request.form['first_name']
+    student.last_name = request.form['last_name']
+    student.house = request.form['house']
+    student.wand = request.form['wand']
+    student.patronus = request.form['patronus']
+    db.session.commit()
+    return redirect(url_for('show', student_id=student.id))
+
+@app.route('/students/<int:student_id>/edit')
+def edit(student_id):
+    student = Student.query.get(student_id)
+    return render_template('edit.html', student=student)
+
 @app.route('/students/<int:student_id>')
 def show(student_id):
     student = Student.query.get(student_id)
     return render_template('show.html', student=student)
+
+@app.route('/students/<int:student_id>', methods=['POST'])
+def update(student_id):
+    student = Student.query.get(student_id)
+    student.first_name = request.form['first_name']
+    student.last_name = request.form['last_name']
+    student.house = request.form['house']
+    student.wand = request.form['wand']
+    student.patronus = request.form['patronus']
+    db.session.commit()
+    return redirect(url_for('show', student_id=student.id))
 
 @app.route('/students/new')
 def new():
